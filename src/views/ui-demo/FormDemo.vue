cd5dbd535325413daa96c25f530e800edb3fd47e
kiccer <1072907338@qq.com>
2023-07-14 12:49:32 +0800

    ✨ feat: 完成 侧边导航

---
+++ src/views/ui-demo/FormDemo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="page-container">
+        This is a Vue component.
+    </div>
+</template>
+
+<script lang="jsx" setup>
+
+</script>
+
+<style lang="scss" scoped>
+// css
+</style>
